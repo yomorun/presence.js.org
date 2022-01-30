51652933c9f99d2c8d11fb52c38643ab3b0ed29a
Hong Xiaojian <xiaojian.hong@gmail.com>
2022-01-30 10:01:02 +0800

    feat: typewriter animation on homepage (#9)
    * fix:fix animation section
    
    * style: restyle the global css
    
    * refactoring codes
    
    * :chore: make animation section
    
    Co-authored-by: Bruno <brunofas12@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import tw from 'tailwind-styled-components';
 import { Header } from '../components/Header';
 import Link from 'next/link';
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Home() {
+	//<First>faster</First>
 	return (
 		<YoMoPresence>
 			<Header />
@@ -12,7 +14,19 @@ export default function Home() {
 					<TitleContainer>
 						<Title className=''>Make a real-time Collaboration</Title>
 						<TitleAnimationContainer>
-							<First>faster</First>
+							<Animation>
+							  <Typewriter
+									words={['better', 'securer', 'faster']}
+									loop={4}
+									// cursor
+									cursorStyle='|'
+									typeSpeed={70}
+									deleteSpeed={50}
+									delaySpeed={1000}
+									// onLoopDone={handleDone}
+									// onType={handleType}
+								/>
+							</Animation>
 							{/* <Second>better</Second>
 							<Third>secure</Third> */}
 						</TitleAnimationContainer>
@@ -20,7 +34,7 @@ export default function Home() {
 					<PresenceInfoContainer>
 						<Message>
 							Presence js is a javascript library helps you build your real-time
-							web application quickly. We also provide a secure, low-latency and
+							web applications quickly. We also provide a secure, low-latency and
 							high-perfomance geo-distributed services to build your apps
 						</Message>
 					</PresenceInfoContainer>
@@ -38,18 +52,17 @@ export default function Home() {
 const YoMoPresence = tw.main`w-full h-screen flex flex-col  bg-black`;
 const YoMoAdmin = tw.section`w-full h-full flex justify-center`;
 const NewYoMoEra = tw.div`w-full lg:w-9/12 h-full  flex justify-center items-center flex-col bg-black`;
-const TitleContainer = tw.div`w-full lg:w-9/12 flex  justify-center flex-col`;
+const TitleContainer = tw.div`w-full lg:w-9/12 flex justify-center flex-col`;
 const Title = tw.div`text-white text-center text-size-title`;
-const TitleAnimationContainer = tw.div`text-title-animation flex items-center justify-center text-white text-center text-size-title`;
-const First = tw.div`text-white text-center text-size-title`;
+const TitleAnimationContainer = tw.div`text-title-animation flex  h-20 items-center justify-center text-white text-center text-size-title`;
+const Animation = tw.div`text-white text-center text-size-title`;
 const Second = tw.div`text-white text-center text-size-title`;
 const Third = tw.div`text-white text-center text-size-title`;
 
 const PresenceInfoContainer = tw.div`w-full lg:w-9/12 mt-12 mb-12 px-12 flex  justify-center flex-col`;
-const Message = tw.span`text-white text-center text-size-message text-2xl`;
+const Message = tw.span`text-white text-center text-size-message text-2xl text-message-color`;
 
 const ButtonContainer = tw.div`w-56 h-14 mb-12 flex  justify-center box2 bg-black `;
 const Button = tw.button` text-center  text-sm bg-gradient-to-r from-cyan-500 to-blue-500 px-4 text-white animate-character rounded-full `;
 
-// const Wrapper = tw.section`w-full h-40 bg-yellow-400 flex justify-center  bg-black cursor-pointer text-white`
-// const NavContainer = tw.div`w-9/12 h-full  flex justify-between items-center cursor-pointer `
+
