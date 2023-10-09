4c3eb71da0d959c8807aa890162b9243e1917529
C.C <fanweixiao@gmail.com>
2023-10-09 21:28:47 +0800

    update group-hug page

--- components/GroupHug.tsx
+++ components/GroupHug.tsx
@@ -3,23 +3,23 @@
 import GroupHug from "@yomo/group-hug-react";
 import "@yomo/group-hug-react/style.css";
 import { createPresence } from "@yomo/presence";
+import { nanoid } from 'nanoid';
 import { useRouter } from "next/router";
 import { useEffect, useRef, useState } from "react";
 
 export default () => {
-  const id = useRef<string>(Math.random().toString());
+  const id = useRef<string>(nanoid());
   const [name, setName] = useState<string>("");
   const presence = useRef(null);
   const router = useRouter();
 
   useEffect(() => {
     if (!presence.current) {
-      const p = createPresence("https://prscd2.allegro.earth/v1", {
+      presence.current = createPresence("https://prscd2.allegro.earth/v1", {
         publicKey: process.env.NEXT_PUBLIC_PRESENCE_PUBLIC_KEY,
         id: id.current,
         debug: true,
       });
-      presence.current = p;
     }
     return () => {
       presence.current = null;
